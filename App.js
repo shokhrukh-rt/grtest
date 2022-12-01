6e8a1c0aefc5d2cfefce52449f90148c74381df3
shokhrukh-rt <mr.shohruh.1003@gmail.com>
2022-12-02 04:51:43 +0900

    Initial commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import { Home } from "./screens";
+const Stack = createNativeStackNavigator();
+const App = () => {
+	return (
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen
+					name="Home"
+					component={Home}
+					options={{ title: "Welcome" }}
+				/>
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
+};
+
+export default App;
