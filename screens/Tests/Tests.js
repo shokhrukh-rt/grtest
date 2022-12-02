daba0867041c8df5df331a67beadbb3b4da0c823
shokhrukh-rt <mr.shohruh.1003@gmail.com>
2022-12-03 08:15:24 +0900

    fetching and showing tests

--- screens/Tests/Tests.js
+++ screens/Tests/Tests.js
@@ -1,13 +1,48 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, FlatList, ScrollView } from "react-native";
+import React, { useState, useEffect } from "react";
+
+//trim the string with string.trim()
+// replace character with string.replace("character", "")
 
 const Tests = ({ route, navigation }) => {
+	const [loading, setLoading] = useState(true);
+	const [data, setData] = useState([]);
 	const { DiffLevel } = route.params;
 
+	const getTests = async () => {
+		try {
+			const res = await fetch("http://211.208.156.232:5000");
+			const json = await res.json();
+			setData(json);
+		} catch (error) {
+			console.log(error);
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	useEffect(() => {
+		getTests();
+	}, []);
 	return (
-		<View>
-			<Text>Tests</Text>
-		</View>
+		<ScrollView style={{ flex: 1 }}>
+			{loading ? (
+				<Text>Loading</Text>
+			) : (
+				data.map((questions, i) => {
+					return (
+						<View>
+							<Text>{questions.qNumber}</Text>
+
+							<Text>{questions.qText}</Text>
+							{questions.answers.map((answer) => (
+								<Text>{answer.answerText}</Text>
+							))}
+						</View>
+					);
+				})
+			)}
+		</ScrollView>
 	);
 };
 
